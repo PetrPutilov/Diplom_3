925918f30d62e5277b2e77de6a252bb1e3c41794
PetrPutilov <putilov_11@icloud.com>
2024-08-12 17:06:32 +0300

    follow from user profile to constructor tests

--- src/main/java/pages/UserProfilePage.java
+++ src/main/java/pages/UserProfilePage.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class UserProfilePage extends BasePage {
 
     private final By nameInput = By.xpath(".//input[@name='Name']");
+    private final By constructorButton = By.xpath(".//a/p[text()='Конструктор']");
+    private final By logo = By.xpath(".//div[@class='AppHeader_header__logo__2D0X2']/a");
 
     public UserProfilePage(WebDriver driver) {
         super(driver, "/account/profile");
@@ -18,5 +20,12 @@ public class UserProfilePage extends BasePage {
                 .until(ExpectedConditions.visibilityOfElementLocated(nameInput));
     }
 
+    public void clickConstructorButton(){
+        clickElement(constructorButton);
+    }
+
+    public void clickLogo(){
+        clickElement(logo);
+    }
 
 }
